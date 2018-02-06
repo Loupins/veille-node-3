f387792919b7d3e4cebf9e7dba1f7b072835de39
Jason Paquette <jason_paquette56@hotmail.com>
2018-02-06 13:39:24 -0500

    Lecture du fichier JSON

--- Mon_premier_serveur.js
+++ Mon_premier_serveur.js
@@ -3,6 +3,9 @@ http.createServer((request, response) =>
 { 
 	console.log('branchement sur le port 3000')
 	response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+	const fs = require("fs");
+	let data = fs.readFileSync('provinces.json');
+	let traduit = JSON.parse(data);
 	
 	response.end(); 
 }).listen(3000);
